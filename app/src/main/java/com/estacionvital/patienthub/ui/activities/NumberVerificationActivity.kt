83f3825823d79e53208fc5773c8973de58d4fa51
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-02-23 12:12:43 -0600

    Se agrega parte del dise;o de la verificacion de numero

--- app/src/main/java/com/estacionvital/patienthub/ui/activities/NumberVerificationActivity.kt
+++ app/src/main/java/com/estacionvital/patienthub/ui/activities/NumberVerificationActivity.kt
@@ -3,11 +3,17 @@ package com.estacionvital.patienthub.ui.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.estacionvital.patienthub.R
+import com.estacionvital.patienthub.ui.views.INumberVerificationView
 
-class NumberVerificationActivity : BaseActivity() {
+class NumberVerificationActivity : BaseActivity(), INumberVerificationView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_verification)
+        supportActionBar?.hide()
+    }
+
+    override fun verifyNumber() {
+        //metodo para verificar el numero
     }
 }
---
+++ app/src/main/java/com/estacionvital/patienthub/ui/views/INumberVerificationView.kt
@@ -0,0 +1,8 @@
+package com.estacionvital.patienthub.ui.views
+
+/**
+ * Created by dusti on 23/02/2018.
+ */
+interface INumberVerificationView {
+    fun verifyNumber(): Unit
+}
