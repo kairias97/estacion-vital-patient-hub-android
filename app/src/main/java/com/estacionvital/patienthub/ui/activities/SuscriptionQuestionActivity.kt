f4fbe9184a42efbde0e734ad55598b5059feed3c
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-02-26 13:20:51 -0600

    Se completa flujo de la pantalla de confirmacion

--- app/src/main/java/com/estacionvital/patienthub/ui/activities/SuscriptionQuestionActivity.kt
+++ app/src/main/java/com/estacionvital/patienthub/ui/activities/SuscriptionQuestionActivity.kt
@@ -3,14 +3,30 @@ package com.estacionvital.patienthub.ui.activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.estacionvital.patienthub.R
 import com.estacionvital.patienthub.ui.views.ISuscriptionQuestionActivity
 
 class SuscriptionQuestionActivity : BaseActivity(), ISuscriptionQuestionActivity {
 
+    private lateinit var mConfirmSuscriptionBtn: Button
+    private lateinit var mCancelSuscriptionBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suscription_question)
+
+        //setting buttons
+        mConfirmSuscriptionBtn = findViewById<Button>(R.id.button_accept)
+        mCancelSuscriptionBtn = findViewById<Button>(R.id.button_cancel)
+
+        //events
+        mConfirmSuscriptionBtn.setOnClickListener{
+            navigateToClubSuscription()
+        }
+        mCancelSuscriptionBtn.setOnClickListener{
+            closeApp()
+        }
     }
 
     override fun navigateToClubSuscription() {
