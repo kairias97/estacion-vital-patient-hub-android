d99d54d98218bf37cb276a4b39d7f49b28f8bb65
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-02-25 21:08:10 -0600

    Se agrega dise;o de pantalla de confirmacion de registro

--- app/src/main/java/com/estacionvital/patienthub/ui/activities/SuscriptionQuestionActivity.kt
+++ app/src/main/java/com/estacionvital/patienthub/ui/activities/SuscriptionQuestionActivity.kt
@@ -1,13 +1,25 @@
 package com.estacionvital.patienthub.ui.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.estacionvital.patienthub.R
+import com.estacionvital.patienthub.ui.views.ISuscriptionQuestionActivity
 
-class SuscriptionQuestionActivity : AppCompatActivity() {
+class SuscriptionQuestionActivity : BaseActivity(), ISuscriptionQuestionActivity {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suscription_question)
+    }
+
+    override fun navigateToClubSuscription() {
+        val clubSuscriptionIntent: Intent = Intent(this, ClubSuscriptionActivity::class.java)
+        clubSuscriptionIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(clubSuscriptionIntent)
+    }
+
+    override fun closeApp() {
+        finish()
     }
 }
