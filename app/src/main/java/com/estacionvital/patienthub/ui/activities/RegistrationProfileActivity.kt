cd4e4c4d3096c2fed3dab1fc33eba0158c708fd6
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-02-28 15:06:37 -0600

    Se a;ade pantalla de registro de perfil, falta funcionalidad solamente

--- app/src/main/java/com/estacionvital/patienthub/ui/activities/RegistrationProfileActivity.kt
+++ app/src/main/java/com/estacionvital/patienthub/ui/activities/RegistrationProfileActivity.kt
@@ -9,5 +9,7 @@ class RegistrationProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_profile)
+
+        supportActionBar!!.title = "Registro de Perfil"
     }
 }
